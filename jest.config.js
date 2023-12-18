d36cb9fd92adc5c1c74dd07988733b7923b56305
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-18 09:07:58 +0530

    test: added setup for test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+   preset: 'ts-jest',
+   testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js'],
+    collectCoverage: true,
+    coverageDirectory: './coverage',
+}
---
+++ tests/utils/authentication.test.ts
@@ -0,0 +1,15 @@
+import { authentication, randomString } from '../../src/utils/authentication'
+
+describe('authentication', () => {
+   it('should return random string', () => {
+      const result = randomString()
+      expect(result).toBeDefined()
+   })
+
+   it('should return hash', () => {
+      const expectedHash =
+         '7675a053edd7b99903b556a518d7b7ef64f94a6f0d9fe54bf8db70afbd24c94d'
+      const result = authentication('password', 'salt')
+      expect(result).toEqual(expectedHash)
+   })
+})
