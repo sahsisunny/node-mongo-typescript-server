2c55cc198a2cd33b6a49553a5c6af803b3a67493
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-18 09:54:09 +0530

    doc: updated readme and added doc comment in the code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,26 @@
 import express from 'express'
 import authentication from './authentication'
 import user from './users'
+import { Request, Response } from 'express'
 
 const router = express.Router()
-
+/**
+ * @route GET /
+ * @description Welcome message and API information
+ * @access Public
+ */
+router.get('/', (req: Request, res: Response) => {
+   res.status(200).json({
+      message: 'Welcome to the Node TypeScript Express Mongoose API!',
+      version: '1.0.0',
+      description:
+         'This API serves as the backend for the Node TypeScript Express Mongoose Starter project. It is built with Node, Express, MongoDB, Mongoose, and TypeScript.',
+      author: 'Sunny Sahsi',
+      repository: 'https://github.com/sahsisunny/node-mongo-typescript-server',
+      documentation:
+         'https://github.com/sahsisunny/node-mongo-typescript-server/blob/master/README.md',
+   })
+})
 export default (): express.Router => {
    authentication(router)
    user(router)
--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -3,24 +3,20 @@ import crypto from 'crypto'
 const length = 128
 const SECRET = 'MOONWALKER'
 
+/**
+ * @description Generates a random string
+ * @returns {string} Random string
+ */
 export const randomString = (): string => {
    return crypto.randomBytes(length).toString('base64')
 }
 
-// export const authentication = {
-//   hash: (password: string, salt: string): string => {
-//     return crypto
-//       .pbkdf2Sync(password, salt, 10000, 512, "sha512")
-//       .toString("hex");
-//   },
-//   salt: (): string => {
-//     return randomString();
-//   },
-//   compare: (password: string, hash: string, salt: string): boolean => {
-//     return authentication.hash(password, salt) === hash;
-//   },
-// };
-
+/**
+ * @description Hashes the password with the salt
+ * @param password
+ * @param salt
+ * @returns {string} Hashed password
+ */
 export const authentication = (password: string, salt: string): string => {
    return crypto
       .createHmac('sha256', [salt, password].join('/'))
